5132431d071cea39670d4afffeb46abbb627576b
twant <twant@kippaustin.org>
2017-02-08 08:48:17 -0500

    finish model

---
+++ models/model.rb
@@ -0,0 +1,11 @@
+def choose_gem(month)
+  if month=="January"
+    gem="Garnet"
+  elsif month=="February"
+    gem="Amethyst"
+  #complete the rest of the method here!
+  end
+
+  return gem 
+end
+
